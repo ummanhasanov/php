166da8ea382119339aa65e2a137de7834c644cb2
user <user@UmmanPHP>
2022-07-15 18:30:20 +0400

    mymodpayment finished and api.php already

--- PozNumMult.php
+++ PozNumMult.php
@@ -15,17 +15,17 @@ class PozNumMult {
 $check = new PozNumMult();
 echo $check->within(49);
 
-class PozNumMult {
-
-    public function NumMult($num) {
-        if ($num % 3 == 0 and $num % 7 == 0) { // hem 7,hem 3 bolunen
-            return $num . 'True';
-        } else {
-            return $num . 'False';
-        }
-    }
-
-}
-
-$check = new PozNumMult();
-echo $check->within(21);
+//class PozNumMult2 {
+//
+//    public function NumMult2($num) {
+//        if ($num % 3 == 0 and $num % 7 == 0) { // hem 7,hem 3 bolunen
+//            return $num . 'True';
+//        } else {
+//            return $num . 'False';
+//        }
+//    }
+//
+//}
+//
+//$check2 = new PozNumMult();
+//echo $check2->within(21);
--- curltest1.php
+++ curltest1.php
@@ -2,11 +2,20 @@
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, 'https://google.com');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, 'http://www.google.com');
 
-$result = curl_exec($ch);
 if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
-curl_close($ch); 
+
+# Setup request to send json via POST.
+$payload = json_encode(array("customer" => $data));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+# Return response instead of printing.
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+# Send request.
+$result = curl_exec($ch);
+curl_close($ch);
+# Print response.
+echo "<pre>$result</pre>";
